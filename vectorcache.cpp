d989623db86214267534f67fb01190d06c169195
burakcanbaz <66463969+burakcanbaz@users.noreply.github.com>
2021-04-07 10:50:04 +0300

    Add files via upload

---
+++ vectorcache.cpp
@@ -0,0 +1,163 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using std::vector;
+
+struct Datas{
+	int n;
+	double fn;
+};
+
+typedef struct Datas calculatedValues;
+
+class BaseFunctionCalculator{
+
+public:
+	virtual ~BaseFunctionCalculator(){}
+	virtual void setCacheSize(int) = 0;
+	virtual double calculate(int) = 0;
+	virtual int getCacheElement(int) = 0;
+
+};
+
+class MyFunctionCalculator : public BaseFunctionCalculator{
+
+	int capacity;
+	int size;
+	int lastSearched;
+	vector<calculatedValues> vectorArray;
+
+	int swap(vector<Datas>& vec, unsigned int swapIndex, int n){
+
+		if(vec[swapIndex].n == n && vec[swapIndex].n != vec[0].n){
+			calculatedValues temp = vec[swapIndex-1];
+			vec[swapIndex-1] = vec[swapIndex];
+			vec[swapIndex] = temp;
+		}
+		return vec[swapIndex-1].n;
+	}
+
+	int search(int n){
+
+		for(unsigned int index = 0 ; index < vectorArray.size(); index++){
+
+			if(vectorArray[index].n == n){
+
+				if(lastSearched == n){
+					lastSearched = swap(vectorArray,index,n);
+					return 1;
+				}
+				lastSearched = vectorArray[index].n;
+				return 1;
+			}
+		}
+		return 0;
+
+	}
+
+	void add(int n,double result){
+
+		if(size != capacity ){
+			vectorArray.insert(vectorArray.begin(), calculatedValues{n,result});
+			size++;
+		}
+		else{
+			vectorArray.pop_back();
+			vectorArray.insert(vectorArray.begin(), calculatedValues{n,result});
+		}
+
+	}
+
+
+public:
+
+	MyFunctionCalculator(){
+
+		lastSearched = 0;
+		capacity = 0;
+		size = 0;
+
+	}
+
+	~MyFunctionCalculator() override{}
+
+
+	void setCacheSize(int size) override{
+
+		capacity = size;
+
+	}
+
+	double calculate(int n) override{
+
+		int found = search(n);
+
+		if(found != 0)
+			return n;
+
+		double result = 0;
+
+		for(int i = 0; i <= n; ++i){
+			result += (sin(i)*i);
+		}
+		add(n,result);
+
+		return result;
+
+	}
+
+	int getCacheElement(int index) override{
+
+		int vecIndex = 0;
+
+		if(index > (capacity-1))
+			return -1;
+
+
+		while(index > 0){
+			vecIndex++;
+			index--;
+		}
+
+		return vectorArray.at(vecIndex).n;
+
+	}
+
+	friend std::ostream& operator<<(std::ostream& out, MyFunctionCalculator& obj){
+
+		std::string delimeter = "";
+		for(unsigned int i = 0; i < obj.vectorArray.size(); i++){
+			std::cout << delimeter << obj.vectorArray[i].n << " ";
+			std::cout << "f(" << obj.vectorArray[i].fn << ")";
+			delimeter = ", ";
+		}
+		return out;
+
+	}
+
+};
+
+
+int main(){
+
+	MyFunctionCalculator myCalculator;
+
+	myCalculator.setCacheSize(3);
+	myCalculator.calculate(5);
+	myCalculator.calculate(20);
+	myCalculator.calculate(10);
+	myCalculator.calculate(5);
+	myCalculator.calculate(5);
+	myCalculator.calculate(5);
+	myCalculator.calculate(2);
+	myCalculator.calculate(3);
+	std::cout << myCalculator << std::endl;
+	std::cout << std::endl;
+	std::cout << myCalculator.getCacheElement(0) << std::endl;
+	std::cout << myCalculator.getCacheElement(1) << std::endl;
+	std::cout << myCalculator.getCacheElement(2) << std::endl;
+	std::cout << myCalculator.getCacheElement(3) << std::endl;
+
+
+}
